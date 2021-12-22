20f7acbf2d9aea990deb1b89ec44ac27d5d1cc5f
tubi55 <tubi55@nate.com>
2021-12-22 15:28:56 +0900

    state설명

--- src/components/Panels.js
+++ src/components/Panels.js
@@ -5,8 +5,20 @@ const len = arr.length;
 const deg = 360/len;
 const path = process.env.PUBLIC_URL;
 
+/*
+  useState hook을 이용하면 state로 자주 변경되는 값을 관리 가능
+  hook은 무조건 컴포넌트 함수 안쪽에서 실행
+  state값이 변경되면 해당 state값의 영향을 받는 모든 JSX가상DOM이 자동으로 재 랜더딩됨
+  
+  사용법
+  useState()함수 호출시 인수로 관리할 데이터값을 넣어서 실행하면 리턴값으로 배열을 반환
+  첫번째 배열값 - state로 관리되는 값
+  두번째 배열값 - 해당 state를 변경할수 있는 함수
+  state값은 무조건 state변경함수를 통해서만 변경가능 (재 랜더링 가능)
+*/
 
 function Panels(){
+
   let [names, setNames] = useState(arr);
   
   const changeState=()=>{
